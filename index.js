305a6ccd26cb98f7ba0e93ad3bcb6b22ebd12147
Kiril <kiril@webvision360.com>
2025-03-21 00:48:56 +0200

    Add endpoint for Game item creation

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
+const client = axios.create({
+    baseURL: 'https://api.hubspot.com',
+    headers: {
+        Authorization: `Bearer ${ process.env.PRIVATE_APP_TOKEN }`,
+        'Content-Type': 'application/json',
+    },
+});
+
+const retrieveCustomObjectItems = async (objId, properties) => client.post(
+    `/crm/v3/objects/${ objId }`,
+    {
+        properties,
+    },
+).then(resp => resp.data);
+
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
@@ -11,6 +26,29 @@ app.use(express.json());
 
 app.get('/update-cobj', (req, res) => {
     res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });      
+});
+
+app.post('/update-cobj', async (req, res) => {
+    try {
+        const data = await retrieveCustomObjectItems(
+            process.env.OBJECT,
+            {
+                name: req.body.name,
+                genre: req.body.genre,
+                platforms: req.body.platforms,
+            },
+        );
+    
+        if (data?.id) {
+            res.redirect('/');
+        } else {
+            throw new Error(data?.errors?.[0].message || 'Error occured');
+        }
+    }
+    catch (e) {
+        console.error(e);
+        res.redirect('/update-cobj');
+    }   
 });
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
